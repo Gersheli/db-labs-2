17ea39ec617534563c5d8db260aae038c212ef4a
Victor <victorshunko@mail.ru>
2024-02-06 23:58:03 +0300

    add anonimous block

--- lab1.sql
+++ lab1.sql
@@ -2,3 +2,16 @@ CREATE TABLE MyTable (
   id NUMBER,
   val NUMBER
 );
+
+DECLARE
+  v_id NUMBER;
+  v_val NUMBER;
+BEGIN
+  FOR i IN 1..10000 LOOP
+    v_id := ROUND(DBMS_RANDOM.VALUE(1, 10000));
+    v_val := ROUND(DBMS_RANDOM.VALUE(1, 100));
+    INSERT INTO MyTable (id, val) VALUES (v_id, v_val);
+  END LOOP;
+  COMMIT;
+END;
+
