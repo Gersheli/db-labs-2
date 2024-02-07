2c165528892d2d24d249d788ec919204deed5074
Victor <victorshunko@mail.ru>
2024-02-07 12:56:07 +0300

    add compensation

--- lab1.sql
+++ lab1.sql
@@ -121,3 +121,43 @@ END;
 BEGIN
   DeleteRecord(1);
 END;
+
+-- 6. Создайте функцию, вычисляющую общее вознаграждение за год. На вход функции 
+-- подаются значение месячной зарплаты и процент годовых премиальных. В общем 
+-- случае общее вознаграждение= (1+ процент годовых премиальных)*12* значение 
+-- месячной зарплаты. При этом предусмотреть что процент вводится как целое число, 
+-- и требуется преобразовать его к дробному. Предусмотреть защиту от ввода 
+-- некорректных данных.
+CREATE OR REPLACE FUNCTION CalculateTotalCompensation(
+  p_monthly_salary NUMBER,
+  p_annual_bonus_percentage INTEGER
+) RETURN NUMBER
+IS
+  v_annual_bonus_percentage NUMBER;
+  v_total_compensation NUMBER;
+BEGIN
+  IF p_annual_bonus_percentage < 0 OR p_annual_bonus_percentage > 100 THEN
+    RAISE_APPLICATION_ERROR(-20001, 'Процент должен быть в диапазоне от 0 до 100');
+  END IF;
+
+  v_annual_bonus_percentage := p_annual_bonus_percentage / 100;
+
+  v_total_compensation := (1 + v_annual_bonus_percentage) * 12 * p_monthly_salary;
+
+  RETURN v_total_compensation;
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('Ошибка: ' || SQLERRM);
+    RETURN NULL;
+END;
+
+DECLARE
+  v_monthly_salary NUMBER := 5000;
+  v_annual_bonus_percentage INTEGER := 10;
+  v_result NUMBER;
+BEGIN
+  v_result := CalculateTotalCompensation(v_monthly_salary, v_annual_bonus_percentage);
+  IF v_result IS NOT NULL THEN
+    DBMS_OUTPUT.PUT_LINE('Итоговая компенсация за год: ' || v_result);
+  END IF;
+END;
